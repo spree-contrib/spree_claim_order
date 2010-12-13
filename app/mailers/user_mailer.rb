f7a8094c46ff18d7b64b6668615ecb40fd604749
Zac Williams <zac@railsdog.com>
2010-12-13 13:01:20 -0600

    confirmation email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
 
   def confirmation_instructions(user)
+
+    @user = user
+
     mail(:to => user.email,
          :subject => Spree::Config[:site_name] + ' ' + t("confirmation_instructions"))
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_scope :user do
-    post "/user_confirmation" => "user_confirmations#create", :as => :user_confirmation
-    get "/user_confirmation/:confirmation_token" => "user_confirmations#show"
+    post "/user_confirmation" => "user_confirmations#create", :as => :create_user_confirmation
+    get "/user_confirmation/:confirmation_token" => "user_confirmations#show", :as => :user_confirmation
   end
 
   match 'orders/:id/claim' => 'orders#claim', :via => :put, :as => :claim_order
