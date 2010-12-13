b2aecdd7faa1cf63e9445d76adc3b1ac69d8717b
Zac Williams <zac@railsdog.com>
2010-12-13 12:10:21 -0600

    fixes issue with claiming orders

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,15 +1,19 @@
+require 'spree_claim_order_ability'
+
 OrdersController.class_eval do
 
   skip_before_filter :check_authorization, :only => [:claim]
 
   def claim
     order = Order.find_by_number params[:id]
-    authorize! :claim, order
+    authorize!(:claim, order)
+
     if order.assign_to_rightful_owner
       flash[:notice] = t('order_claim_success')
     else
       flash[:error] = t('order_claim_failure')
     end
+
     redirect_to account_path
   end
 
--- app/models/spree_claim_order_ability.rb
+++ app/models/spree_claim_order_ability.rb
@@ -1,12 +1,13 @@
 class SpreeClaimOrderAbility
   include CanCan::Ability
 
+
   def initialize(user)
-    #############################
     can :claim, Order do |order|
       user.confirmed? && (user.email.downcase == order.email.downcase)
     end
   end
+
 end
 
 Ability.register_ability(SpreeClaimOrderAbility)
