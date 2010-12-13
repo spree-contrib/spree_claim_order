02abb00df923af0bcfa9ee4d05aa73c7fc68d164
Zac Williams <zac@railsdog.com>
2010-12-13 11:18:08 -0600

    removing un-needed UsersController action and cooresponding routes/specs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
     get "/user_confirmation/:confirmation_token" => "user_confirmations#show"
   end
 
-  match 'send_confirmation_instructions' => 'users#send_confirmation_instructions', :via => :put
   match 'orders/:id/claim' => 'orders#claim', :via => :put, :as => :claim_order
 end
