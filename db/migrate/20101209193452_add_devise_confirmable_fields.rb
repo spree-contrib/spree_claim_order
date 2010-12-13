de053755e57821f25bb86aad0ba243a919ab6873
Zac Williams <zac@railsdog.com>
2010-12-13 14:21:41 -0600

    update to migration

--- db/migrate/20101209193452_add_devise_confirmable_fields.rb
+++ db/migrate/20101209193452_add_devise_confirmable_fields.rb
@@ -3,6 +3,13 @@ class AddDeviseConfirmableFields < ActiveRecord::Migration
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :confirmation_token, :string
+
+    User.all.each do |user|
+      unless user.email =~ /example.[net|com]/
+        user.confirmation_sent_at = user.created_at
+        user.save
+      end
+    end
   end
 
   def self.down
