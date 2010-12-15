d1c4174ecb9e5f7729380e418fe872dc48dce564
Zac Williams <zac@railsdog.com>
2010-12-15 13:44:24 -0600

    adding a spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,6 +50,12 @@ describe User do
         @user.save
       end
 
+      it "should claim any unclaimed orders if confirmed" do
+        @user.stub(:confirmed_at => Time.now)
+        @user.should_receive(:claim_all_unclaimed_orders)
+        @user.save
+      end
+
       context "email address does not change" do
 
         it "should not change confirmed_at" do
