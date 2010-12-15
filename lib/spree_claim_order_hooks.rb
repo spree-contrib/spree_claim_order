d95d44095b8917ce5500a34c407a942209dd558b
Zac Williams <zac@railsdog.com>
2010-12-15 14:13:23 -0600

    removing un-needed template and hook

--- lib/spree_claim_order_hooks.rb
+++ lib/spree_claim_order_hooks.rb
@@ -1,4 +1,3 @@
 class ClaimOrderHooks < Spree::ThemeSupport::HookListener
   insert_before :account_summary, 'users/resend_confirmation'
-  insert_after :account_my_orders, 'users/unclaimed_orders'
 end
