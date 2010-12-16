0b2668da547ed5beab10872223e2022884f7748f
Zac Williams <zac@railsdog.com>
2010-12-16 07:51:08 -0600

    just some comments

--- lib/claim_order_configuration.rb
+++ lib/claim_order_configuration.rb
@@ -1,5 +1,10 @@
 class ClaimOrderConfiguration < Configuration
 
+  # Setting this to false will allow you to bypass email confirmation requirements
+  # and orders will automatically be associated with existing user accounts even
+  # if the account's email address has not been confirmed.
+  #
+  # Restart the app if you change this preference
   preference :require_email_confirmation, :boolean, :default => true
 
 end
