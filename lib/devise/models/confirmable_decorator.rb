6a667e175e19361b83f33b8bbb6bc8ccbc352eee
Zac Williams <zac@railsdog.com>
2010-12-20 15:50:33 -0600

    abandoning alias approach for now

--- lib/devise/models/confirmable_decorator.rb
+++ lib/devise/models/confirmable_decorator.rb
@@ -2,16 +2,18 @@ module Devise
   module Models
     Confirmable.module_eval do
 
-      alias :devise_confirm! :confirm!
       def confirm!
-        devise_confirm! && claim_all_unclaimed_orders
+        unless_confirmed do
+          self.confirmation_token = nil
+          self.confirmed_at = Time.now
+          save(:validate => false)
+        end && claim_all_unclaimed_orders && true
       end
 
       protected
 
-      alias :devise_confirmation_required? :confirmation_required?
       def confirmation_required?
-        User.devise_modules.include?(:confirmable) && !unclaimed_orders.empty? && devise_confirmation_required?
+        User.devise_modules.include?(:confirmable) && !unclaimed_orders.empty? && !confirmed?
       end
 
     end
