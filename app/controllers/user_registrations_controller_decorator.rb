a927d24b72e8404f2aeba786d111d9e88b49c5b1
Zac Williams <zac@railsdog.com>
2010-12-21 12:42:28 -0600

    claim unclaimed after registration

--- app/controllers/user_registrations_controller_decorator.rb
+++ app/controllers/user_registrations_controller_decorator.rb
@@ -1,6 +1,9 @@
 UserRegistrationsController.class_eval do
 
+  after_filter :claim_unclaimed, :only => :create, :if => Spree::ClaimOrder::Config[:require_email_confirmation]
   after_filter :unclaimed_orders_flash, :only => :create
+
+   private
 
   private
 
@@ -8,6 +11,10 @@ UserRegistrationsController.class_eval do
     if @user.confirmation_token and @user.unclaimed_orders.present?
       flash[:error] = t("instructions_sent_at_registration")
     end
+  end
+
+  def claim_unclaimed
+    @user.claim_all_unclaimed_orders
   end
 
 end
